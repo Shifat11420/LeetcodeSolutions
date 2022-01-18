61d242a2a07da7178ad1c310aa7590ddfd0f6664
Shifat11420 <shifat.perveen@gmail.com>
2022-01-18 11:12:44 -0500

    linked list problems

--- LinkedLists.py
+++ LinkedLists.py
@@ -5,11 +5,7 @@ class Node:
 
 class LinkedList:
     def __init__(self) :
-        self.head = None             #createt empty linkedlist
-
-    def insert_at_beginning(self, data):    
-        node = Node(data,self.head)
-        self.head = node
+        self.head = None             #create empty linkedlist
 
     def print(self):
         itr = self.head
@@ -31,8 +27,11 @@ class LinkedList:
             itr = itr.next     
         return count      
 
-    def insert_at_end(self, data):
+    def insert_at_beginning(self, data):    
+        node = Node(data,self.head)
+        self.head = node    
 
+    def insert_at_end(self, data):
         if self.head == None:
             self.head = Node(data,None)
             return
@@ -44,7 +43,7 @@ class LinkedList:
 
     def insert_at(self, index, data):    
         if index < 0 or index > self.get_length():
-            raise Exception 
+            raise Exception("Invalid Index") 
 
         if index == 0:
             self.insert_at_beginning(data)
@@ -63,7 +62,7 @@ class LinkedList:
 
     def remove_at(self, index):
         if index <0 or index > self.get_length():
-            raise Exception
+            raise Exception("Invalid Index")
 
         if index == 0:
             self.head = self.head.next
@@ -80,10 +79,74 @@ class LinkedList:
     def insert_values(self, data_list):
         self.head = None
         for data in data_list:
-            self.insert_at_end(data)             
-     
+            self.insert_at_end(data)      
+
+    def insert_after_value(self, data_after, data_to_insert):
+        # Search for first occurance of data_after value in linked list
+        itr = self.head
+        count = 0
+        while itr:
+            if itr.data == data_after:
+                print("data_after found at index ", count)
+                break
+            itr = itr.next
+            count += 1
+        # Now insert data_to_insert after data_after node
+        self.insert_at(count+1, data_to_insert)
+
+    def remove_by_value(self, data):
+        # Remove first node that contains data 
+        itr = self.head
+        count = 0
+        while itr:   
+            if itr.data == data:
+                print("first node that contains data is at index ", count)
+                break
+            itr = itr.next
+            count +=1     
+
+        if itr:           
+            self.remove_at(count)    
+        else:
+            raise Exception("data not found")
+
+
+    '''   solution given
+
+        def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data==data_after:
+            self.head.next = Node(data_to_insert,self.head.next)
+            return
+
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+
+            itr = itr.next
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+    '''        
+
 if __name__== '__main__':
-    root = LinkedList()
+    #root = LinkedList()
     # root.insert_at_beginning(5)
     # root.insert_at_beginning(10)
     # #root.insert_at_beginning(15)
@@ -98,7 +161,23 @@ if __name__== '__main__':
     # root.print()
     # root.remove_at(3)
     # root.print()
-    root.insert_values(["mango", "banana", "apple", "grapes"])
-    root.insert_at(1,"pineapple")
-    root.print()
+    # root.insert_values(["mango", "banana", "apple", "grapes"])
+    # root.insert_at(1,"pineapple")
+    # root.print()
+    # root.insert_after_value("apple", "lichi")
+    # root.print()
+    ll = LinkedList()
+    ll.insert_values(["banana","mango","grapes","orange"])
+    ll.print()
+    ll.insert_after_value("mango","apple") # insert apple after mango
+    ll.print()
+    ll.remove_by_value("orange") # remove orange from linked list
+    ll.print()
+    # ll.remove_by_value("figs")
+    # ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
     pass
