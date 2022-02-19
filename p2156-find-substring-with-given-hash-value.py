3b707d3e55c68b1e59cde42741a155058cb27242
Shifat11420 <shifat.perveen@gmail.com>
2022-02-19 00:28:59 -0500

    all saved

--- p2156-find-substring-with-given-hash-value.py
+++ p2156-find-substring-with-given-hash-value.py
@@ -1,3 +1,23 @@
+# The hash of a 0-indexed string s of length k, given integers p and m, is computed using the following function:
+
+# hash(s, p, m) = (val(s[0]) * p0 + val(s[1]) * p1 + ... + val(s[k-1]) * pk-1) mod m.
+# Where val(s[i]) represents the index of s[i] in the alphabet from val('a') = 1 to val('z') = 26.
+
+# You are given a string s and the integers power, modulo, k, and hashValue. Return sub, the first substring of s of length k such that hash(sub, power, modulo) == hashValue.
+
+# The test cases will be generated such that an answer always exists.
+
+# A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+# Example 1:
+
+# Input: s = "leetcode", power = 7, modulo = 20, k = 2, hashValue = 0
+# Output: "ee"
+# Explanation: The hash of "ee" can be computed to be hash("ee", 7, 20) = (5 * 1 + 5 * 7) mod 20 = 40 mod 20 = 0. 
+# "ee" is the first substring of length 2 with hashValue 0. Hence, we return "ee".
+
 
 # import collections
 
--- p415-add-string.py
+++ p415-add-string.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/add-strings/
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
 
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
 def addString(num1,num2):
     num1 = list(num1)
     num2 = list(num2)
