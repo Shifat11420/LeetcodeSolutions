de7acd7a6a2b23df1aecc738f04d40779a714085
Shifat11420 <67601996+Shifat11420@users.noreply.github.com>
2023-12-30 11:16:58 -0500

    misc

---
+++ misc/p1897.py
@@ -0,0 +1,40 @@
+# You are given an array of strings words (0-indexed).
+# In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+# Return true if you can make every string in words equal using any number of operations, and false otherwise.
+
+# Example 1:
+# Input: words = ["abc","aabc","bc"]
+# Output: true
+# Explanation: Move the first 'a' in words[1] to the front of words[2],
+# to make words[1] = "abc" and words[2] = "abc".
+# All the strings are now equal to "abc", so return true.
+
+# Example 2:
+# Input: words = ["ab","a"]
+# Output: false
+# Explanation: It is impossible to make all the strings equal using the operation.
+ 
+# Time: O(n*m), Space : O(n)
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        count = {}
+        n = len(words)
+        if n==1:
+            return True
+        for w in words:
+            for c in w:
+                count[c] = 1 + count.get(c,0)
+
+        for i in count.values():
+            if i%n:
+                return False
+        return True                
+
+    def makeEqual(self, words: List[str]) -> bool:
+        joint = "".join(words)
+        frq = set(joint)
+
+        for i in frq:
+            if joint.count(i)%len(words)!=0:
+                return False
+        return True        
