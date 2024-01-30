732d07a3fa8e45a3733e3bd6dfada01df35bbeb9
Shifat11420 <67601996+Shifat11420@users.noreply.github.com>
2024-01-30 14:40:43 -0500

    misc

---
+++ misc/p1160.py
@@ -0,0 +1,27 @@
+# You are given an array of strings words and a string chars.
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+# Return the sum of lengths of all good strings in words.
+
+# Example 1:
+# Input: words = ["cat","bt","hat","tree"], chars = "atach"
+# Output: 6
+# Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+# Example 2:
+# Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+# Output: 10
+# Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+# Time: O(mn), Space: O(1)
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        res = 0
+        for w in words:
+            good = True
+            for c in w:
+                if chars.count(c) < w.count(c):
+                    good = False
+                    break
+            if good:             
+                res += len(w)        
+        return res 
