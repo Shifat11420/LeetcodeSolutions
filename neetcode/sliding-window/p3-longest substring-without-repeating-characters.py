c2acf6bfd72fb91726ddea6e93bdf3c192821aed
Shifat11420 <67601996+Shifat11420@users.noreply.github.com>
2024-02-20 18:22:20 -0500

    correction on sliding window

--- neetcode/sliding-window/p3-longest substring-without-repeating-characters.py
+++ neetcode/sliding-window/p3-longest substring-without-repeating-characters.py
@@ -11,18 +11,30 @@
 #Complexity: time O(n), space O(n) (for set)
 # two pointer
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:        
+    def lengthOfLongestSubstring(self, s: str) -> int:  #with set      
         l = 0
         charSet = set()    # use set to find duplicates
         res = 0
 
         for r in range(len(s)):
-            if s[r] in charSet:
+            while s[r] in charSet:
                 charSet.remove(s[l])
                 l += 1
             charSet.add(s[r])    
             res = max(res, r-l+1)
         return res
+
+    def lengthOfLongestSubstring(self, s: str) -> int:  #with hm
+        hm = {}
+        maxlen = 0
+        l, r = 0, 0
+        while r<len(s):
+            if s[r] in hm:
+                l = max(l, hm[s[r]])                
+            hm[s[r]] = r+1    
+            maxlen = max(maxlen, r-l+1)   
+            r+=1        
+        return maxlen    
 
 s = "abcabcbb"
 new = Solution
