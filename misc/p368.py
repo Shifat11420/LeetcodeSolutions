197a476a51c561d5340006889482e74864245e76
Shifat11420 <67601996+Shifat11420@users.noreply.github.com>
2024-02-24 13:12:27 -0500

    misc

---
+++ misc/p368.py
@@ -0,0 +1,56 @@
+# Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+# answer[i] % answer[j] == 0, or
+# answer[j] % answer[i] == 0
+# If there are multiple solutions, return any of them.
+
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: [1,2]
+# Explanation: [1,3] is also accepted.
+
+# Example 2:
+# Input: nums = [1,2,4,8]
+# Output: [1,2,4,8]
+ 
+class Solution:
+    # memoization, recursion # Time: O(n^2), Space: O(n)
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        cache = {}   # longest subset starting at i
+
+        def dfs(i):
+            if i == len(nums): return []
+            if i in cache: return cache[i]
+
+            res = [nums[i]]
+            for j in range(i+1, len(nums)):
+                if nums[j]%nums[i]==0:
+                    tmp = [nums[i]]+dfs(j)
+                    if len(tmp)>len(res):
+                        res = tmp
+            cache[i] = res 
+            return res
+
+        res = []
+        for i in range(len(nums)):
+            tmp = dfs(i)           
+            if len(tmp)>len(res):
+                res = tmp
+        return res        
+
+class Solution:
+    # DP, tabular # Time: O(n^2), Space: O(n)
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        dp = [[n] for n in nums]
+        res = []
+
+        for i in reversed(range(len(nums))):
+            for j in range(i+1, len(nums)):
+                if nums[j]%nums[i]==0:
+                    tmp = [nums[i]] + dp[j]
+                    if len(tmp)>len(dp[i]):
+                        dp[i] = tmp
+            if len(dp[i])>len(res):
+                res = dp[i]
+        return res
